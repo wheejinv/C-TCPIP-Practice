248e9b410ed2398f2ee00ed4583d0eba4d545e75
whee <wheejinv@naver.com>
2018-01-16 04:13:31 +0900

    server: 오탈자로 인한 찾기 힘든 버그 수정ㅋㅋ client: init

--- Linux/01_소켓프로그래밍시작/hello_server.c
+++ Linux/01_소켓프로그래밍시작/hello_server.c
@@ -5,7 +5,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-void error_handling(char *messages);
+void error_handling(char *message);
 
 int  main(int argc, char *argv[])
 {
@@ -18,26 +18,23 @@ int  main(int argc, char *argv[])
 
   char message[] = "Hello World!";
 
-  if (argc != 2)
-  {
+  if (argc != 2) {
     printf("Usage : %s <port>\n", argv[0]);
     exit(1);
   }
 
   serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
-  if (serv_sock == -1)
-  {
+  if (serv_sock == -1) {
     error_handling("socket() error");
   }
 
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family      = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port        = htonl(atoi(argv[1]));
+  serv_addr.sin_port        = htons(atoi(argv[1]));
 
-  if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
-  {
+  if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
     error_handling("bind() error");
   }
 
@@ -50,8 +47,7 @@ int  main(int argc, char *argv[])
                           (struct sockaddr *)&clnt_addr,
                           &clnt_addr_size);
 
-  if (clnt_sock == -1)
-  {
+  if (clnt_sock == -1) {
     error_handling("accept() error");
   }
 
@@ -60,7 +56,7 @@ int  main(int argc, char *argv[])
   close(serv_sock);
 
   return 0;
-} /* main */
+}
 
 void error_handling(char *message)
 {
