f560d0fe1cc6d3d9c977ed2c1e156713b4cd85c2
whee <wheejinv@naver.com>
2018-02-21 03:12:18 +0900

    link update

--- Window/20_Windows에서의 쓰레드 동기화_5_ChatServer/chat_serv_win.c
+++ Window/20_Windows에서의 쓰레드 동기화_5_ChatServer/chat_serv_win.c
@@ -21,105 +21,110 @@ HANDLE hMutex;
 
 int main(int argc, char *argv[])
 {
-	WSADATA wsaData;
-	SOCKET hServSock, hClntSock;
-	SOCKADDR_IN servAdr, clntAdr;
+  WSADATA wsaData;
+  SOCKET  hServSock, hClntSock;
+  SOCKADDR_IN servAdr, clntAdr;
 
-	int clntAdrSz;
-	HANDLE hThread;
-
-	
-	if (argc != 2) {
-		printf("Usage : %s <port>\n", argv[0]);
-		exit(1);
-	}
-
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		ErrorHandling("WSAStartup() error");
-	}
-
-	hMutex = CreateMutex(NULL, FALSE, NULL);
-	hServSock = socket(PF_INET, SOCK_STREAM, 0);
-	if (hServSock == INVALID_SOCKET) {
-		ErrorHandling("socket() error");
-	}
-
-	memset(&servAdr, 0, sizeof(servAdr));
-	servAdr.sin_family = AF_INET;
-	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAdr.sin_port = htons(atoi(argv[1]));
-
-	if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
-		ErrorHandling("bind() error");
-	}
-
-	if (listen(hServSock, 5) == SOCKET_ERROR) {
-		ErrorHandling("listen() error");
-	}
+  int clntAdrSz;
+  HANDLE hThread;
 
 
-	while (1)
-	{
-		clntAdrSz = sizeof(clntAdr);
-		hClntSock = accept(hServSock, (struct sockaddr*)&clntAdr, &clntAdrSz);
+  if (argc != 2) {
+    printf("Usage : %s <port>\n", argv[0]);
+    exit(1);
+  }
 
-		WaitForSingleObject(hMutex, INFINITE);
-		clntSocks[clntCnt++] = hClntSock;
-		ReleaseMutex(hMutex);
+  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    ErrorHandling("WSAStartup() error");
+  }
 
-		hThread = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void *)&hClntSock, 0, NULL);
+  hMutex    = CreateMutex(NULL, FALSE, NULL);
+  hServSock = socket(PF_INET, SOCK_STREAM, 0);
 
-		printf("Connected client IP : %s \n", inet_ntoa(clntAdr.sin_addr));
-	}
+  if (hServSock == INVALID_SOCKET) {
+    ErrorHandling("socket() error");
+  }
 
-	closesocket(hServSock);
-	WSACleanup();
-	return 0;
+  memset(&servAdr, 0, sizeof(servAdr));
+  servAdr.sin_family      = AF_INET;
+  servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servAdr.sin_port        = htons(atoi(argv[1]));
+
+  if (bind(hServSock, (SOCKADDR *)&servAdr, sizeof(servAdr)) == SOCKET_ERROR) {
+    ErrorHandling("bind() error");
+  }
+
+  if (listen(hServSock, 5) == SOCKET_ERROR) {
+    ErrorHandling("listen() error");
+  }
+
+  while (1)
+  {
+    clntAdrSz = sizeof(clntAdr);
+    hClntSock = accept(hServSock, (struct sockaddr *)&clntAdr, &clntAdrSz);
+
+    WaitForSingleObject(hMutex, INFINITE);
+    clntSocks[clntCnt++] = hClntSock;
+    ReleaseMutex(hMutex);
+
+    hThread = (HANDLE)_beginthreadex(NULL,
+                                     0,
+                                     HandleClnt,
+                                     (void *)&hClntSock,
+                                     0,
+                                     NULL);
+
+    printf("Connected client IP : %s \n", inet_ntoa(clntAdr.sin_addr));
+  }
+
+  closesocket(hServSock);
+  WSACleanup();
+  return 0;
 }
 
 unsigned WINAPI HandleClnt(void *arg)
 {
-	SOCKET hClntSock = *((SOCKET *)arg);
-	int strLen = 0, i;
-	char msg[BUF_SIZE];
+  SOCKET hClntSock = *((SOCKET *)arg);
+  int    strLen = 0, i;
+  char   msg[BUF_SIZE];
 
-	while ((strLen = recv(hClntSock, msg, BUF_SIZE, 0)) != 0) {
-		SendMsg(msg, strLen);
-	}
+  while ((strLen = recv(hClntSock, msg, BUF_SIZE, 0)) != 0) {
+    SendMsg(msg, strLen);
+  }
 
-	WaitForSingleObject(hMutex, INFINITE);
-	for (i = 0; i<clntCnt; i++)	// remove disconnected client
-	{
-		if (hClntSock == clntSocks[i])
-		{
-			while (i++<clntCnt - 1)
-				clntSocks[i] = clntSocks[i + 1];
-			break;
-		}
-	}
-	clntCnt--;
-	ReleaseMutex(hMutex);
-	closesocket(clntSocks);
-	return NULL;
+  WaitForSingleObject(hMutex, INFINITE);
+
+  for (i = 0; i < clntCnt; i++) // remove disconnected client
+  {
+    if (hClntSock == clntSocks[i])
+    {
+      while (i++ < clntCnt - 1) clntSocks[i] = clntSocks[i + 1];
+      break;
+    }
+  }
+  clntCnt--;
+  ReleaseMutex(hMutex);
+  closesocket(clntSocks);
+  return NULL;
 }
-
 
 // send to all
 void SendMsg(char *msg, int len)
 {
-	int i;
-	WaitForSingleObject(hMutex, INFINITE);
-	for (i = 0; i < clntCnt; i++) {
-		send(clntSocks[i], msg, len, 0);
-	}
-		
-	ReleaseMutex(hMutex);
-}
+  int i;
 
+  WaitForSingleObject(hMutex, INFINITE);
+
+  for (i = 0; i < clntCnt; i++) {
+    send(clntSocks[i], msg, len, 0);
+  }
+
+  ReleaseMutex(hMutex);
+}
 
 void ErrorHandling(char *msg)
 {
-	fputs(msg, stderr);
-	fputc('\n', stderr);
-	exit(1);
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(1);
 }
